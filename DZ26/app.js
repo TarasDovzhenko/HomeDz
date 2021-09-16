3ddcd19c5d8f7795459cdef8d6e40c2437316587
TarasDovzhenko <jonnitar03@gmail.com>
2021-09-16 14:34:41 +0300

    DZ26

--- DZ26/app.js
+++ DZ26/app.js
@@ -1,5 +1,6 @@
-var moment = require("moment"); // require
-console.log(moment().format()); // "2014-09-08T08:02:17-05:00" (ISO 8601, no fractional seconds)
-console.log(moment().format("dddd, MMMM Do YYYY, h:mm:ss a")); // "Sunday, February 14th 2010, 3:25:50 pm"
-console.log(moment().format("ddd, hA")); // "Sun, 3PM"
-console.log(moment().format("[Today is] dddd")); // "Today is Sunday"
+import * as calculator from "./calculator.js";
+
+console.log(calculator.add(10, 2)); // 12
+console.log(calculator.sub(12, 5)); // 7
+console.log(calculator.mult(7, 4)); // 28
+console.log(calculator.div(28, 2)); // 14
